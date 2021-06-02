7bd56e0dfe8c885622eb3d358f6e26add941ddae
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-06-02 15:21:25 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+window.addEventListener('scroll', () => {
+  const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+  let addBox = () => {
+    const item = document.createElement("div");
+    item.setAttribute("class", "box");
+    container.appendChild(item);
+  }
