b10a795133ab95f5e6c6f1ed7cf18d51197ec906
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-06-07 20:45:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const container = document.querySelector(".container");
 window.addEventListener('scroll', () => {
   const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
   let addBox = () => {
-    const item = document.createElement("div");
-    item.setAttribute("class", "box");
-    container.appendChild(item);
-  }
-  if (scrollTop + clientHeight === scrollHeight) {
-      addBox();
+    for (let i = 0; i < 33; i++) {
+      const item = document.createElement("div");
+      item.setAttribute("class", "box");
+      container.appendChild(item);
     }
-  })
+}
+
+if (scrollTop + clientHeight === scrollHeight) {
+    addBox();
+  }
+})
